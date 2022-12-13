0deaa49c9d5ed3a6c3a0dc61fce24c8a06da39cc
Gabi Movila <gabimovila16@gmail.com>
2022-12-13 22:52:48 +0200

    Added DB connection

---
+++ backend/src/config/db.ts
@@ -0,0 +1,14 @@
+import * as mongoose from 'mongoose';
+
+const dbConnectionString = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(dbConnectionString);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.log(`Can t connect to db: ${err}`);
+    process.exit(1);
+  }
+};
+export default connectToDB;
